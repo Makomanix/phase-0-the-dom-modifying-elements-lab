83f1cb525a5b6bbc6a3b623acb1d01b4606b8c8a
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-24 17:39:06 -0400

    commit tree 2d394ebb260616c995bea74f2e3227a2e85249e5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your code here!
+// const element = document.createElement('div');
+// document.body.append(element);
+
+// const ul = document.createElement('ul');
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul);
+
+// const element = document.getElementById('main');
+// element.style.height = '300px';
+// element.style.backgroundColor = '#27647B';
+// element.textContent = "You\'ve changed what\'s on the screen!"
+// element.style.fontSize = '122px';
+// element.style.marginLeft = '30px';
+// element.style.lineHeight = 2;
+// element.style.marginRight = '30px';
+
+// element.className = "pet-listing dog";
+
+// element.classList.remove('dog');
+// element.classList.add('cat', 'sale')
+
+// const main = document.querySelector('main');
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = ('victory');
+// newHeader.textContent = 'Jon is the champion'
+
+const main = document.querySelector('#main');
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Jon is the champion'
+console.log(newHeader)
